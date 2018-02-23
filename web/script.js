a8286bdd08e7577a834166dbd26acaf8d2942968
engsulta <en.ahmedsultan@gmail.com>
2018-02-23 20:50:53 +0200

    updates

--- web/script.js
+++ web/script.js
@@ -1,52 +1,51 @@
+var lastrecieved = 0;
 $(document).ready(function () {
-    var lastrecievedmasg=0;
-   // var lastrecievedmasg=0;
-    initcontact();
+    lastrecieved = 0;
     initchat();
-    $("#sendbtn").click(sendMessage);
+    initcontact();
 
 });
-//$(document).ready(function(){
-//});
 
-//$(document).ready(setInterval(ajaxcall, 5000));
+$("#sendbtn").click(sendMessage);
+
+
 function initcontact() {
     setInterval(ajaxcallContact, 10000);
 }
 function initchat() {
-    setInterval(ajaxcallChat, 3000);
+    setInterval(ajaxcallChat, 5000);
 }
 function ajaxcallChat() {
-  //  $('.msgRow').remove();
+    $('.msgRow').remove();
     $.ajax({url: "MainServlet", contentType: 'application/json', dataType: 'json', type: 'POST', success: function (result) {
             for (var i = 0; i < result.length; i++) {
                 $('#chatarea').append('<div class="msgrow"><p class="msgname">' + result[i].name + '</p><br><p class="msgbody">' + result[i].body + '</p></div>');
             }
-            lastrecieved=result[result.length-1].id;
+            lastrecieved = result[result.length - 1].id;
 
         }});
 }
 function ajaxcallContact() {
-     $('.contactrow').remove();
-     $.ajax({url: "LoadContacts", contentType: 'application/json', dataType: 'json', type: 'POST', success: function (result) {
+    $('.contactrow').remove();
+    $.ajax({url: "LoadContacts", contentType: 'application/json', dataType: 'json', type: 'POST', success: function (result) {
             for (var i = 0; i < result.length; i++) {
                 $('#contactarea').append('<div class="contactrow"><p class="contactname">' + result[i].name + '</p><p class="contactstatus">' + result[i].status + '</p></div>');
             }
-            
+
 
         }});
 }
 function sendMessage() {
-    var id=lastrecieved;
-    var name = $("#chatOwner").html();
+    var id = lastrecieved;
+    var name = "ahmed";
     var body = $("#txtmsg").val();
     $("#txtmsg").val = "";
 
-    var jsonData = {"id": id,"name": name, "body": body};
+    var jsonData = {"id": id, "name": name, "body": body};
 
     $.ajax({url: "MainServlet", contentType: 'application/json', data: jsonData, dataType: 'json', type: 'GET', success: function (result) {
-//            $("#done").html("");
-//            $("#done").html("done");
+            alert(result.body);//           
+
         }});
 
 }
